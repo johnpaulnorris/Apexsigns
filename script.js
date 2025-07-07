a7cbc2712416c5c5b002d2f264bbfb2518c024cf
johnpaulnorris <42979586+johnpaulnorris@users.noreply.github.com>
2025-07-07 23:13:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-// Full site JS (v4.3)
+// Full site JS (v4.2)
 document.addEventListener('DOMContentLoaded',()=>{
-  console.log('v4.3 real JS loaded');
+  console.log('v4.2 real JS loaded');
   window.addEventListener('scroll',()=>{
     const header=document.querySelector('header');
     header.style.background=window.scrollY>50?'#111':'#000';
